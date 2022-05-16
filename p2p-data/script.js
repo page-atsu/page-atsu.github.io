692c149a7bf60053fdc584a145494cba578d7496
page-atsu <105641884+page-atsu@users.noreply.github.com>
2022-05-16 22:18:41 +0900

    Update script.js

--- p2p-data/script.js
+++ p2p-data/script.js
@@ -1,12 +1,13 @@
 const Peer = window.Peer;
 
 (async function main() {
-  const localVideo = document.getElementById('js-local-stream');
   const localId = document.getElementById('js-local-id');
-  const callTrigger = document.getElementById('js-call-trigger');
+  const localText = document.getElementById('js-local-text');
+  const connectTrigger = document.getElementById('js-connect-trigger');
   const closeTrigger = document.getElementById('js-close-trigger');
-  const remoteVideo = document.getElementById('js-remote-stream');
+  const sendTrigger = document.getElementById('js-send-trigger');
   const remoteId = document.getElementById('js-remote-id');
+  const messages = document.getElementById('js-messages');
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
 
@@ -15,69 +16,82 @@ const Peer = window.Peer;
     SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
   `.trim();
 
-  const localStream = await navigator.mediaDevices
-    .getUserMedia({
-      audio: true,
-      video: true,
-    })
-    .catch(console.error);
-
-  // Render local stream
-  localVideo.muted = true;
-  localVideo.srcObject = localStream;
-  localVideo.playsInline = true;
-  await localVideo.play().catch(console.error);
-
   const peer = (window.peer = new Peer({
     key: window.__SKYWAY_KEY__,
     //key: '39f05a53-160c-4ffd-a468-11cdc3be64ef',
     debug: 3,
   }));
 
-  // Register caller handler
-  callTrigger.addEventListener('click', () => {
+  // Register connecter handler
+  connectTrigger.addEventListener('click', () => {
     // Note that you need to ensure the peer has connected to signaling server
     // before using methods of peer instance.
     if (!peer.open) {
       return;
     }
 
-    const mediaConnection = peer.call(remoteId.value, localStream);
+    const dataConnection = peer.connect(remoteId.value);
 
-    mediaConnection.on('stream', async stream => {
-      // Render remote stream for caller
-      remoteVideo.srcObject = stream;
-      remoteVideo.playsInline = true;
-      await remoteVideo.play().catch(console.error);
+    dataConnection.once('open', async () => {
+      messages.textContent += `=== DataConnection has been opened ===\n`;
+
+      sendTrigger.addEventListener('click', onClickSend);
     });
 
-    mediaConnection.once('close', () => {
-      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
-      remoteVideo.srcObject = null;
+    dataConnection.on('data', data => {
+      messages.textContent += `Remote: ${data}\n`;
     });
 
-    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
+    dataConnection.once('close', () => {
+      messages.textContent += `=== DataConnection has been closed ===\n`;
+      sendTrigger.removeEventListener('click', onClickSend);
+    });
+
+    // Register closing handler
+    closeTrigger.addEventListener('click', () => dataConnection.close(true), {
+      once: true,
+    });
+
+    function onClickSend() {
+      const data = localText.value;
+      dataConnection.send(data);
+
+      messages.textContent += `You: ${data}\n`;
+      localText.value = '';
+    }
   });
 
   peer.once('open', id => (localId.textContent = id));
 
-  // Register callee handler
-  peer.on('call', mediaConnection => {
-    mediaConnection.answer(localStream);
+  // Register connected peer handler
+  peer.on('connection', dataConnection => {
+    dataConnection.once('open', async () => {
+      messages.textContent += `=== DataConnection has been opened ===\n`;
 
-    mediaConnection.on('stream', async stream => {
-      // Render remote stream for callee
-      remoteVideo.srcObject = stream;
-      remoteVideo.playsInline = true;
-      await remoteVideo.play().catch(console.error);
+      sendTrigger.addEventListener('click', onClickSend);
     });
 
-    mediaConnection.once('close', () => {
-      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
-      remoteVideo.srcObject = null;
+    dataConnection.on('data', data => {
+      messages.textContent += `Remote: ${data}\n`;
     });
 
-    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
+    dataConnection.once('close', () => {
+      messages.textContent += `=== DataConnection has been closed ===\n`;
+      sendTrigger.removeEventListener('click', onClickSend);
+    });
+
+    // Register closing handler
+    closeTrigger.addEventListener('click', () => dataConnection.close(true), {
+      once: true,
+    });
+
+    function onClickSend() {
+      const data = localText.value;
+      dataConnection.send(data);
+
+      messages.textContent += `You: ${data}\n`;
+      localText.value = '';
+    }
   });
 
   peer.on('error', console.error);
